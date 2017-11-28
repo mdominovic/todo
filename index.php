e454a49dde45ea8b4d2f6545450f76639d8d4bce
mdominovic <mdominovic1@gmail.com>
2017-11-28 19:45:57 +0100

    minor improvements

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $task = new Tasks();
 
 if(isset($_POST['submit'])) {
     $task->addTask($_POST['task']);
+    header('Location: index.php');
 }
 ?>
 
@@ -15,18 +16,24 @@ if(isset($_POST['submit'])) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <title>Document</title>
 
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-    <h1>TODO list</h1>
+    <h1>To do list</h1>
 
-    <form action="" method="post">
-        <input name="task" type="text" >
-        <input name="submit" type="submit" >
-    </form>
+    <hr>
+
+    <div class="input-group">
+        <form action="" method="post">
+            <p class="title"> Add new task: </p>
+            <input name="task" type="text" >
+            <input name="submit" type="submit">
+        </form>
+    </div>
 
     <hr>
 
@@ -37,6 +44,7 @@ if(isset($_POST['submit'])) {
         </tr>
 
         <?php
+
         $as = $task->displayTask();
         foreach($as as $a):
         ?>
@@ -45,15 +53,15 @@ if(isset($_POST['submit'])) {
             <th class="">
                 <?php echo $a[1]; ?>
             </th>
-            <td>
+            <td class="done-btn">
                 <form method="post" action="">
                     <input type="submit" name="delete" value="Done">
                     <input type="hidden" name="id" value="<?php echo $a[0];?>">
                     <?php
 
                         if(isset($_POST['delete'])) {
-                            $hh = ((int)$_POST['id']);
-                            $task->deleteTask($hh);
+                            $id = ((int)$_POST['id']);
+                            $task->deleteTask($id);
 
                             header('Location: index.php');
 
@@ -61,7 +69,7 @@ if(isset($_POST['submit'])) {
                     ?>
                 </form>
             </td>
-            <td>
+            <td class="t-stamp">
                 <?php echo $a[2]; ?>
             </td>
         </tr>
